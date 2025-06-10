dd523b2c282a813c317d3ac1da8cbc1931275f21
matheustorresdev97 <matheustorresdev@gmail.com>
2025-06-10 20:01:42 -0300

    fix:  component PostFeatured

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { Container } from "@/components/container";
 import { Header } from "@/components/header";
-import { PostCoverImage } from "@/components/post-cover-image";
-import { PostHeading } from "@/components/post-heading";
+import { PostFeatured } from "@/components/post-featured";
 import { PostsList } from "@/components/post-list";
 import { SpinLoader } from "@/components/spin-loader";
 import { Suspense } from "react";
@@ -11,38 +10,10 @@ export default async function Home() {
   return (
     <Container>
       <Header />
-      <section className='grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group'>
-         <PostCoverImage
-          linkProps={{
-            href: '/post/asdfasdf',
-          }}
-          imageProps={{
-            width: 1200,
-            height: 720,
-            src: '/images/bryen_9.png',
-            alt: 'Alt da imagem',
-            priority: true,
-          }}
-        />
-        <div className='flex flex-col gap-4 sm:justify-center'>
-          <time
-            className='text-slate-600 block text-sm/tight'
-            dateTime='2025-04-20'
-          >
-            20/04/2025 10:00
-          </time>
+      <Suspense fallback={<SpinLoader />}>
+        <PostFeatured />
+      </Suspense>
 
-          <PostHeading as='h1' url='#'>
-            Rerum, vel ex? Impedit ullam harum blanditiis
-          </PostHeading>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. In velit
-            dolorem est dolor porro, doloribus neque, quidem mollitia
-            doloremque, ad perspiciatis fugiat. Rerum, vel ex? Impedit ullam
-            harum blanditiis mollitia?
-          </p>
-        </div>
-      </section>
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
       </Suspense>
