c2a059fbb51a40d5f513253cdf2f2028ef317c95
matheustorresdev97 <matheustorresdev@gmail.com>
2025-06-10 19:39:56 -0300

    feat: laoding e suspense para exibir fallback quando algo estiver carregando

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
+import { PostsList } from "@/components/post-list";
 import { SpinLoader } from "@/components/spin-loader";
-import clsx from 'clsx';
+import { Suspense } from "react";
 
 export default async function Home() {
 
   return (
     <div>
-          <SpinLoader containerClasses={clsx('min-h-[500px]', 'bg-amber-500')} />
+          <header>
+        <h1 className='text-6xl font-bold text-center py-8'>Aqui é a HEADER</h1>
+      </header>
+
+      <Suspense fallback={<SpinLoader />}>
+        <PostsList />
+      </Suspense>
+
+      <footer>
+        <p className='text-6xl font-bold text-center py-8'>Footer</p>
+      </footer>
     </div>
   );
 }
--- src/repositories/post/post-repository.ts
+++ src/repositories/post/post-repository.ts
@@ -3,4 +3,4 @@ import { PostModel } from '@/models/post/post-model';
 export interface PostRepository {
     findAll(): Promise<PostModel[]>;
     findById(id: string): Promise<PostModel>;
-}
+} 
