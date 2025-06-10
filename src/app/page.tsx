d991fbb3e35e2aeab18c299e07be1db3fb11fcde
matheustorresdev97 <matheustorresdev@gmail.com>
2025-06-10 19:43:21 -0300

    feat: tema claro e escuro no tailwind css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ import { Suspense } from "react";
 export default async function Home() {
 
   return (
-    <div>
-          <header>
+    <div className='text-slate-900 bg-slate-100 min-h-screen'>
+      <header>
         <h1 className='text-6xl font-bold text-center py-8'>Aqui é a HEADER</h1>
       </header>
 
