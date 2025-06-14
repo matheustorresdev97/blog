43b762695e0456d109be461f98ae9d13570d7420
matheustorresdev97 <matheustorresdev@gmail.com>
2025-06-14 19:24:25 -0300

    feat: static site generation (SSG)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import { Container } from "@/components/container";
-import { Header } from "@/components/header";
 import { PostFeatured } from "@/components/post-featured";
 import { PostsList } from "@/components/post-list";
 import { SpinLoader } from "@/components/spin-loader";
--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,6 +1,6 @@
 import { SinglePost } from '@/components/single-post';
 import { SpinLoader } from '@/components/spin-loader';
-import { findPostBySlugCached } from '@/lib/post/queries';
+import { findAllPublicPostsCached, findPostBySlugCached } from '@/lib/post/queries';
 import { Metadata } from 'next';
 import { Suspense } from 'react';
 
@@ -20,12 +20,25 @@ export async function generateMetadata({
   };
 }
 
+export async function generateStaticParams() {
+  const posts = await findAllPublicPostsCached();
+  const params = posts.map(post => {
+    return {
+      slug: post.slug,
+    };
+  });
+
+  console.log(params);
+
+  return params;
+}
+
 export default async function PostSlugPage({ params }: PostSlugPageProps) {
   const { slug } = await params;
   const post = await findPostBySlugCached(slug);
 
   return (
-  <Suspense fallback={<SpinLoader className='min-h-20 mb-16' />}>
+    <Suspense fallback={<SpinLoader className='min-h-20 mb-16' />}>
       <SinglePost slug={slug} />
     </Suspense>
   );
