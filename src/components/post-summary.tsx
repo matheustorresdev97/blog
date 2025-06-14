2680abb6ee5b1bbe279e129c3228ba4f49abed1a
matheustorresdev97 <matheustorresdev@gmail.com>
2025-06-14 19:13:52 -0300

    style: singlePost

--- src/components/post-summary.tsx
+++ src/components/post-summary.tsx
@@ -1,5 +1,5 @@
-import { formatDatetime, formatDistanceToNow } from '@/utils/format-datetime';
 import { PostHeading } from './post-heading';
+import { PostDate } from './post-date';
 
 
 type PostSummaryProps = {
@@ -19,13 +19,7 @@ export async function PostSummary({
 }: PostSummaryProps) {
   return (
     <div className='flex flex-col gap-4 sm:justify-center'>
-      <time
-        className='text-slate-600 block text-sm/tight'
-        dateTime={createdAt}
-        title={formatDistanceToNow(createdAt)}
-      >
-        {formatDatetime(createdAt)}
-      </time>
+      <PostDate dateTime={createdAt} />
 
       <PostHeading as={postHeading} url={postLink}>
         {title}
