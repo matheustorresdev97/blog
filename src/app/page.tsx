79087e411dd1ae5a7cbfd05927a921390a904510
matheustorresdev97 <matheustorresdev@gmail.com>
2025-06-14 19:11:30 -0300

    feat: usando Suspense para garantir o carregamento rapido

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,11 +9,8 @@ export default async function Home() {
 
   return (
     <>
-      <Suspense fallback={<SpinLoader />}>
+      <Suspense fallback={<SpinLoader className='min-h-20 mb-16' />}>
         <PostFeatured />
-      </Suspense>
-
-      <Suspense fallback={<SpinLoader />}>
         <PostsList />
       </Suspense>
     </>
