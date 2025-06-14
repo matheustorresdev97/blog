6f7419cfdf139b845b0d70ce5df009bebe5aeec6
matheustorresdev97 <matheustorresdev@gmail.com>
2025-06-14 19:45:09 -0300

    feat: criação do DrizzlePostRepository

--- src/repositories/post/json-post-repository.ts
+++ src/repositories/post/json-post-repository.ts
@@ -30,16 +30,12 @@ export class JsonPostRepository implements PostRepository {
   async findAllPublic(): Promise<PostModel[]> {
     await this.simulateWait();
 
-    console.log('\n', 'findAllPublic', '\n');
-
     const posts = await this.readFromDisk();
     return posts.filter(post => post.published);
   }
 
   async findAll(): Promise<PostModel[]> {
     await this.simulateWait();
-
-    console.log('\n', 'findAll', '\n');
 
     const posts = await this.readFromDisk();
     return posts;
@@ -55,8 +51,7 @@ export class JsonPostRepository implements PostRepository {
     return post;
   }
 
-  async findBySlug(slug: string): Promise<PostModel> {
-
+  async findBySlugPublic(slug: string): Promise<PostModel> {
     const posts = await this.findAllPublic();
     const post = posts.find(post => post.slug === slug);
 
