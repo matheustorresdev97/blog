a8ecf58b7796ab16ab25a5dcd72a8ab105ab3bb9
matheustorresdev97 <matheustorresdev@gmail.com>
2025-06-14 19:21:47 -0300

    feat: boundaries com error.tsx

---
+++ src/app/error.tsx
@@ -0,0 +1,24 @@
+
+'use client';
+
+import ErrorMessage from '@/components/error-message';
+import { useEffect } from 'react';
+
+type RootErrorPageProps = {
+    error: Error;
+    reset: () => void;
+};
+
+export default function RootErrorPage({ error }: RootErrorPageProps) {
+    useEffect(() => {
+        console.log(error);
+    }, [error]);
+
+    return (
+        <ErrorMessage
+            pageTitle='Internal Server Error'
+            contentTitle='501'
+            content='Ocorreu um erro do qual nossa aplicação não conseguiu ser recuperar. Tente novamente mais tarde.'
+        />
+    );
+}
