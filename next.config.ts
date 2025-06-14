7cb06a903b08a4701a56a82025baee6119534d11
matheustorresdev97 <matheustorresdev@gmail.com>
2025-06-14 19:58:18 -0300

    Nova diretiva use cache, cacheLife e cacheTag (experimental)

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
+  experimental: {
+    useCache: true,
+    cacheLife: {
+      seconds: {
+        stale: 0,
+        revalidate: 300,
+        expire: 300,
+      },
+    },
   },
 };
-
 export default nextConfig;
--- src/app/exemplo/[id]/page.tsx
+++ src/app/exemplo/[id]/page.tsx
@@ -1,5 +1,5 @@
 import { revalidateExampleAction } from '@/actions/revalidate-exemple';
-import { formatHour } from '@/utils/format-datetime';
+import { formatHour, formatHourCached } from '@/utils/format-datetime';
 
 //export const dynamic = 'force-static';
 //export const revalidate = 10;
@@ -15,22 +15,12 @@ export default async function ExemploDynamicPage({
   params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
-  const hour = formatHour(Date.now());
-
-
-  const response = await fetch('https://randomuser.me/api/?results=1', {
-    next: {
-      tags: ['randomuser'],
-      revalidate: 30,
-    },
-  });
-  const json = await response.json();
-  const name = json.results[0].name.first;
+  const hour = await formatHourCached();
 
   return (
     <main className='min-h-[600px] text-4xl font-bold'>
       <div>
-        Name: {name} | Hora: {hour} | (ID: {id})
+        Hora: {hour} | (ID: {id})
       </div>
 
 
