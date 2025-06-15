b6fa23b6131c96685c658f303fcb59ef38c560dd
matheustorresdev97 <matheustorresdev@gmail.com>
2025-06-15 19:11:11 -0300

    feat: comportamento do suspense com e sem cache

--- src/repositories/post/drizzle-post-repository.ts
+++ src/repositories/post/drizzle-post-repository.ts
@@ -1,10 +1,14 @@
 import { PostModel } from '@/models/post/post-model';
 import { PostRepository } from './post-repository';
 import { drizzleDb } from '@/db/drizzle';
+import { logColor } from '@/utils/log-color';
+import { asyncDelay } from '@/utils/async-delay';
+import { SIMULATE_WAIT_IN_MS } from '@/lib/constants';
 
 export class DrizzlePostRepository implements PostRepository {
   async findAllPublic(): Promise<PostModel[]> {
-    console.log('\n', 'D findAllPublic', '\n');
+    await asyncDelay(SIMULATE_WAIT_IN_MS, true);
+    logColor('findAllPublic', Date.now());
 
     const posts = await drizzleDb.query.posts.findMany({
       orderBy: (posts, { desc }) => desc(posts.createdAt),
@@ -15,7 +19,8 @@ export class DrizzlePostRepository implements PostRepository {
   }
 
   async findBySlugPublic(slug: string): Promise<PostModel> {
-    console.log('\n', 'D findBySlugPublic', '\n');
+    await asyncDelay(SIMULATE_WAIT_IN_MS, true);
+    logColor('findBySlugPublic', Date.now());
 
     const post = await drizzleDb.query.posts.findFirst({
       where: (posts, { eq, and }) =>
@@ -28,7 +33,8 @@ export class DrizzlePostRepository implements PostRepository {
   }
 
   async findAll(): Promise<PostModel[]> {
-    console.log('\n', 'D findAll', '\n');
+    await asyncDelay(SIMULATE_WAIT_IN_MS, true);
+    logColor('findAll', Date.now());
     const posts = await drizzleDb.query.posts.findMany({
       orderBy: (posts, { desc }) => desc(posts.createdAt),
     });
@@ -37,7 +43,8 @@ export class DrizzlePostRepository implements PostRepository {
   }
 
   async findById(id: string): Promise<PostModel> {
-    console.log('\n', 'D findById', '\n');
+    await asyncDelay(SIMULATE_WAIT_IN_MS, true);
+    logColor('findById', Date.now());
     const post = await drizzleDb.query.posts.findFirst({
       where: (posts, { eq }) => eq(posts.id, id),
     });
