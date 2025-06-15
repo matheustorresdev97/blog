4519f7185aea7840b33335deb9a2dc91fef040d9
matheustorresdev97 <matheustorresdev@gmail.com>
2025-06-15 19:49:27 -0300

    feat: add chalk

--- src/utils/log-color.ts
+++ src/utils/log-color.ts
@@ -1,8 +1,8 @@
-import { styleText } from 'util';
+import chalk from 'chalk'; // Or import { green, bgGreen, whiteBright } from 'chalk'; for specific colors
 
 export function logColor(...msg: (string | number)[]) {
   const messages = msg
-    .map(message => styleText(['bgGreen', 'whiteBright'], `${message}`))
+    .map(message => chalk.bgGreen.whiteBright(`${message}`)) // Use chalk for styling
     .join(' ');
-  console.log(styleText('green', messages));
+  console.log(chalk.green(messages)); // Use chalk for the final message color
 }
