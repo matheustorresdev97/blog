0bca29604e95b4183dd0ab986881503cbdcac43b
matheustorresdev97 <matheustorresdev@gmail.com>
2025-06-23 18:55:33 -0300

    feat: validando o FormDate e exibindo erros no front

--- src/actions/post/create-post-action.ts
+++ src/actions/post/create-post-action.ts
@@ -1,6 +1,9 @@
 "use server";
 
-import { PublicPost } from "@/dto/post/dto";
+import { makePartialPublicPost, PublicPost } from "@/dto/post/dto";
+import { PostCreateSchema } from "@/lib/post/queries/validations";
+import { PostModel } from "@/models/post/post-model";
+import { getZodErrorMessages } from "@/utils/get-zod-error-messages";
 
 type CreatePostActionState = {
   formState: PublicPost;
@@ -19,9 +22,27 @@ export async function createPostAction(
   }
 
   const formDataToObj = Object.fromEntries(formData.entries());
+  const zodParsedObj = PostCreateSchema.safeParse(formDataToObj);
+
+  if (!zodParsedObj.success) {
+    const errors = getZodErrorMessages(zodParsedObj.error.format());
+    return {
+      errors,
+      formState: makePartialPublicPost(formDataToObj),
+    };
+  }
+
+  const validPostData = zodParsedObj.data;
+  const newPost: PostModel = {
+    ...validPostData,
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+    id: Date.now().toString(),
+    slug: Math.random().toString(36),
+  };
 
   return {
-    formState: prevState.formState,
+    formState: newPost,
     errors: [],
   };
 }
--- src/components/admin/manage-post-form.tsx
+++ src/components/admin/manage-post-form.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useActionState, useState } from "react";
+import { useActionState, useEffect, useState } from "react";
 import { Button } from "../button";
 import { InputCheckbox } from "../input-checkbox";
 import { InputText } from "../input-text";
@@ -8,6 +8,7 @@ import { MarkdownEditor } from "../markdown-editor";
 import { ImageUploader } from "./image-uploader";
 import { makePartialPublicPost, PublicPost } from "@/dto/post/dto";
 import { createPostAction } from "@/actions/post/create-post-action";
+import { toast } from "react-toastify";
 
 type ManagePostFormProps = {
   publicPost?: PublicPost;
@@ -21,84 +22,91 @@ export function ManagePostForm({ publicPost }: ManagePostFormProps) {
 
   const [state, action, isPending] = useActionState(
     createPostAction,
-    initialState,
+    initialState
   );
 
+  useEffect(() => {
+    if (state.errors.length > 0) {
+      toast.dismiss();
+      state.errors.forEach((error) => toast.error(error));
+    }
+  }, [state.errors]);
+
   const { formState } = state;
-  const [contentValue, setContentValue] = useState(publicPost?.content || '');
+  const [contentValue, setContentValue] = useState(publicPost?.content || "");
 
   return (
-    <form action={action} className='mb-16'>
-      <div className='flex flex-col gap-6'>
+    <form action={action} className="mb-16">
+      <div className="flex flex-col gap-6">
         <InputText
-          labelText='ID'
-          name='id'
-          placeholder='ID gerado automaticamente'
-          type='text'
+          labelText="ID"
+          name="id"
+          placeholder="ID gerado automaticamente"
+          type="text"
           defaultValue={formState.id}
           readOnly
         />
 
         <InputText
-          labelText='Slug'
-          name='slug'
-          placeholder='Slug gerada automaticamente'
-          type='text'
+          labelText="Slug"
+          name="slug"
+          placeholder="Slug gerada automaticamente"
+          type="text"
           defaultValue={formState.slug}
           readOnly
         />
 
         <InputText
-          labelText='Autor'
-          name='author'
-          placeholder='Digite o nome do autor'
-          type='text'
+          labelText="Autor"
+          name="author"
+          placeholder="Digite o nome do autor"
+          type="text"
           defaultValue={formState.author}
         />
 
         <InputText
-          labelText='Título'
-          name='title'
-          placeholder='Digite o título'
-          type='text'
+          labelText="Título"
+          name="title"
+          placeholder="Digite o título"
+          type="text"
           defaultValue={formState.title}
         />
 
         <InputText
-          labelText='Excerto'
-          name='excerpt'
-          placeholder='Digite o resumo'
-          type='text'
+          labelText="Excerto"
+          name="excerpt"
+          placeholder="Digite o resumo"
+          type="text"
           defaultValue={formState.excerpt}
         />
 
         <MarkdownEditor
-          labelText='Conteúdo'
+          labelText="Conteúdo"
           value={contentValue}
           setValue={setContentValue}
-          textAreaName='content'
+          textAreaName="content"
           disabled={false}
         />
 
         <ImageUploader />
 
         <InputText
-          labelText='URL da imagem de capa'
-          name='coverImageUrl'
-          placeholder='Digite a url da imagem'
-          type='text'
+          labelText="URL da imagem de capa"
+          name="coverImageUrl"
+          placeholder="Digite a url da imagem"
+          type="text"
           defaultValue={formState.coverImageUrl}
         />
 
         <InputCheckbox
-          labelText='Publicar?'
-          name='published'
-          type='checkbox'
+          labelText="Publicar?"
+          name="published"
+          type="checkbox"
           defaultChecked={formState.published}
         />
 
-        <div className='mt-4'>
-          <Button type='submit'>Enviar</Button>
+        <div className="mt-4">
+          <Button type="submit">Enviar</Button>
         </div>
       </div>
     </form>
