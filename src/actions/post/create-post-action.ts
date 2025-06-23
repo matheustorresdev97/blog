83255adcbc5f5a6a2f1d3120e4c0db2b409b2cd8
matheustorresdev97 <matheustorresdev@gmail.com>
2025-06-23 18:50:22 -0300

    feat: convertendo FormData em Object

--- src/actions/post/create-post-action.ts
+++ src/actions/post/create-post-action.ts
@@ -11,10 +11,17 @@ export async function createPostAction(
   prevState: CreatePostActionState,
   formData: FormData
 ): Promise<CreatePostActionState> {
-  const title = formData.get("title")?.toString() || "";
+  if (!(formData instanceof FormData)) {
+    return {
+      formState: prevState.formState,
+      errors: ["Dados inválidos"],
+    };
+  }
+
+  const formDataToObj = Object.fromEntries(formData.entries());
 
   return {
-    formState: { ...prevState.formState, title },
+    formState: prevState.formState,
     errors: [],
   };
 }
