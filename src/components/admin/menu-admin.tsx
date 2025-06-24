ea1aca4867d5d44e8e81e43b85749abe505aecac
matheustorresdev97 <matheustorresdev@gmail.com>
2025-06-24 16:39:04 -0300

    feat: criando logout para sair, apagar cookie e o JWT

--- src/components/admin/menu-admin.tsx
+++ src/components/admin/menu-admin.tsx
@@ -1,50 +1,62 @@
-'use client';
+"use client";
 
-import clsx from 'clsx';
+import { logoutAction } from "@/actions/login/logout-action";
+import clsx from "clsx";
 import {
   CircleXIcon,
   FileTextIcon,
+  HourglassIcon,
   HouseIcon,
+  LogOutIcon,
   MenuIcon,
   PlusIcon,
-} from 'lucide-react';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import { useEffect, useState } from 'react';
+} from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { startTransition, useEffect, useState, useTransition } from "react";
 
 export function MenuAdmin() {
   const [isOpen, setIsOpen] = useState(false);
   const pathname = usePathname();
+  const [isPending, startTransition] = useTransition();
 
   useEffect(() => {
     setIsOpen(false);
   }, [pathname]);
 
   const navClasses = clsx(
-    'bg-slate-900 text-slate-100 rounded-lg',
-    'flex flex-col  mb-8',
-    'sm:flex-row sm:flex-wrap',
-    !isOpen && 'h-10',
-    !isOpen && 'overflow-hidden',
-    'sm:overflow-visible sm:h-auto',
+    "bg-slate-900 text-slate-100 rounded-lg",
+    "flex flex-col  mb-8",
+    "sm:flex-row sm:flex-wrap",
+    !isOpen && "h-10",
+    !isOpen && "overflow-hidden",
+    "sm:overflow-visible sm:h-auto"
   );
   const linkClasses = clsx(
-    '[&>svg]:w-[16px] [&>svg]:h-[16px] px-4',
-    'flex items-center justify-start gap-2 cursor-pointer',
-    'transition hover:bg-slate-800 rounded-lg',
-    'h-10',
-    'shrink-0',
+    "[&>svg]:w-[16px] [&>svg]:h-[16px] px-4",
+    "flex items-center justify-start gap-2 cursor-pointer",
+    "transition hover:bg-slate-800 rounded-lg",
+    "h-10",
+    "shrink-0"
   );
   const openCloseBtnClasses = clsx(
     linkClasses,
-    'text-blue-200 italic',
-    'sm:hidden',
+    "text-blue-200 italic",
+    "sm:hidden"
   );
+
+  function handleLogout(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+    e.preventDefault();
+
+    startTransition(async () => {
+      await logoutAction();
+    });
+  }
 
   return (
     <nav className={navClasses}>
       <button
-        onClick={() => setIsOpen(s => !s)}
+        onClick={() => setIsOpen((s) => !s)}
         className={openCloseBtnClasses}
       >
         {!isOpen && (
@@ -62,20 +74,36 @@ export function MenuAdmin() {
         )}
       </button>
 
-      <a className={linkClasses} href='/' target='_blank'>
+      <a className={linkClasses} href="/" target="_blank">
         <HouseIcon />
         Home
       </a>
 
-      <Link className={linkClasses} href='/admin/post'>
+      <Link className={linkClasses} href="/admin/post">
         <FileTextIcon />
         Posts
       </Link>
 
-      <Link className={linkClasses} href='/admin/post/new'>
+      <Link className={linkClasses} href="/admin/post/new">
         <PlusIcon />
         Criar post
       </Link>
+
+      <a onClick={handleLogout} href="#" className={linkClasses}>
+        Add commentMore actions
+        {isPending && (
+          <>
+            <HourglassIcon />
+            Aguarde...
+          </>
+        )}
+        {!isPending && (
+          <>
+            <LogOutIcon />
+            Sair
+          </>
+        )}
+      </a>
     </nav>
   );
 }
